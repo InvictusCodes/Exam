a4c1f7bfb6625f1f0554fe06dc4b962684508b72
Vishal Bambhaniya <invictuscodes@gmail.com>
2025-05-07 16:23:30 +0530

    Create Module4 Q9.c

---
+++ Module4 Q9.c
@@ -0,0 +1,30 @@
+WAP to check whether the entered string is palindrome or not.
+Code:
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char string1[20];
+    int i,length,flag=0;
+    printf("Enter a string:");
+    scanf("%s",string1);
+    length=strlen(string1);
+    for(i=0;i<length;i++){
+        if(string1[i]!=string1[length-i-1])
+        {
+            flag=1;
+            break;
+        }
+
+    }
+    if(flag)
+    {
+        printf("%s is not a Palindrome.",string1);
+
+    }
+    else{
+        printf("%s is  a Palindrome.",string1);
+
+    }
+    return 0;
+}
